0a4df650880e2692a3a2eef95912a98113c8801c
tuanvinhh <142334752+tuanvinhh@users.noreply.github.com>
2024-02-27 16:05:15 +0700

    sua tren nhanh main

--- test05.cpp
+++ test05.cpp
@@ -2,7 +2,8 @@
 
 int main() {
 	std::cout << "Hello World!";
-	std::cout << "Test thêm text!";
-	std::cout << "thêm text sau khi dã t?o nhánh m?i!";
+	std::cout << "Test thÃªm text!";
+	std::cout << "thÃªm text sau khi dÃ£ t?o nhÃ¡nh m?i!";
+	std::cout << "test conflict ( sua tren nhanh main)!";
     return 0;
 }
