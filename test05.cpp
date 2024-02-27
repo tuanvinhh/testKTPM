f770d609ad1e594fb87c494f96443bde070c4b25
tuanvinhh <2051012135vinh@ou.edu.vn>
2024-02-27 15:17:14 +0700

    them txt moi vao nanh feature-new

--- test05.cpp
+++ test05.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello World!";
+	std::cout << "Hello World!";
 	std::cout << "Test thêm text!";
+	std::cout << "thêm text sau khi dã t?o nhánh m?i!";
     return 0;
 }
